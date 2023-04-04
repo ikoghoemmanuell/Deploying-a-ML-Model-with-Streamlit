af64300b9daa9a825412bbcd797ceae8d8b14368
ikoghoemmanuell <oke.kogo@gmail.com>
2023-04-04 01:17:46 +0100

    fixed model and success

--- decisionTree.py
+++ decisionTree.py
@@ -1,4 +1,4 @@
-# fix the errors "1 has type int, but expected one of: bytes, unicode' in the streamlit app below:
+# make this streamlit sales prediction app look more beautiful by adding a background and other styles:
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -10,15 +10,23 @@ data = pd.read_csv('merged_train_data.csv')
 # Split the data
 X = data.drop('sales', axis=1)
 
-# Load the model and encoder
+# Load the model and encoder ans scaler
 model = pickle.load(open("model.pkl", "rb"))
 encoder = pickle.load(open("encoder.pkl", "rb"))
+scaler = pickle.load(open("scaler.pkl", "rb"))
+
+
+    # merged3=merged3.set_index(['date'])
 
 # Define the function to make predictions
-def predict_sales(input_data):
-    cols = ['family', 'city', 'state', 'cluster', 'type_y', 'type_x']
-    for col in cols:
-        input_data[col] = encoder.transform(input_data[col])
+def predict_sales(input_data, input_df):
+    # defining categories and numeric columns
+    categoric_columns = ['family', 'city', 'state', 'type_y', 'type_x']
+    columns = list(input_df.columns) 
+    numeric_columns = [i for i in columns if i not in categoric_columns]
+    scaled_num = scaler.fit_transform(input_df[numeric_columns])
+    encoded_cat = encoder.transform(input_df[categoric_columns])
+    input_data = pd.concat([scaled_num, encoded_cat], axis=1)
     # convert input_data to a numpy array before flattening to convert it back to a 2D array
     input_data = input_data.to_numpy()
     prediction = model.predict(input_data.flatten().reshape(1, -1))
@@ -46,21 +54,11 @@ family = st.selectbox('Family', list(X['family'].unique()))
 type_x = st.radio('Type X', list(X['type_x'].unique()))
 type_y = st.radio('Type Y', list(X['type_y'].unique()))
 onpromotion = st.selectbox('On Promotion', [True, False])
+oil_price = st.number_input("Enter oil price", format="%.5f")
 
 st.write('## Date Information')
-year = st.number_input('Year')
-month = st.number_input('Month', min_value=1, max_value=12)
-day_of_month = st.number_input('Day of Month', min_value=1, max_value=31)
-day_of_week = st.number_input('Day of Week', min_value=1, max_value=7)
-day_of_year = st.number_input('Day of Year', min_value=1, max_value=365)
-week_of_year = st.number_input('Week of Year', min_value=1, max_value=52)
-quarter = st.number_input('Quarter', min_value=1, max_value=4)
-is_weekend = st.selectbox('Is Weekend', ['True', 'False'])
-is_month_start = st.selectbox('Is Month Start', ['True', 'False'])
-is_month_end = st.selectbox('Is Month End', ['True', 'False'])
-is_quarter_start = st.selectbox('Is Quarter Start', ['True', 'False'])
-is_quarter_end = st.selectbox('Is Quarter End', ['True', 'False'])
-is_year_start = st.selectbox('Is Year Start', ['True', 'False'])
+# INPUT DATE, THEN USE GETDATEFEATURES TO EXTRACT ALL THE DATE INFORMATION
+date = st.date_input("Pick a date")
 
 # Convert input parameters to a pandas DataFrame
 input_dict = {
@@ -72,25 +70,44 @@ input_dict = {
    'type_x': type_x,
    'type_y': type_y,
    'onpromotion': onpromotion,
-   'year': year,
-   'month': month,
-   'day_of_month': day_of_month,
-   'day_of_week': day_of_week,
-   'day_of_year': day_of_year,
-   'week_of_year': week_of_year,
-   'quarter': quarter,
-   'is_weekend': is_weekend,
-   'is_month_start': is_month_start,
-   'is_month_end': is_month_end,
-   'is_quarter_start': is_quarter_start,
-   'is_quarter_end': is_quarter_end,
-   'is_year_start': is_year_start
+   'oil_price': oil_price,
+   'date' : date
   }
 
 input_df = pd.DataFrame([input_dict])
 
+def getDateFeatures(df):
+    
+    df['date'] = pd.to_datetime(df['date'], errors='coerce')
+    df['month'] = df['date'].dt.month
+    df['day_of_month'] = df['date'].dt.day
+    df['day_of_year'] = df['date'].dt.dayofyear
+    df['week_of_year'] = df['date'].dt.isocalendar().week
+    df['week_of_year'] = df['week_of_year'].astype(float)
+    df['day_of_week'] = df['date'].dt.dayofweek
+    df['year'] = df['date'].dt.year
+    df["is_weekend"] = np.where(df['day_of_week'] > 4, 1, 0)
+    df['is_month_start'] = df['date'].dt.is_month_start.astype(int)
+    df['quarter'] = df['date'].dt.quarter
+    df['is_month_end'] = df['date'].dt.is_month_end.astype(int)
+    df['is_quarter_start'] = df['date'].dt.is_quarter_start.astype(int)
+    df['is_quarter_end'] = df['date'].dt.is_quarter_end.astype(int)
+    df['is_year_start'] = df['date'].dt.is_year_start.astype(int)
+    df['is_year_end'] = df['date'].dt.is_year_end.astype(int)
+
+    df["season"] = np.where(df.month.isin([12,1,2]), 0, 1)
+    df["season"] = np.where(df.month.isin([6,7,8]), 2, df["season"])
+    df["season"] = pd.Series(np.where(df.month.isin([9, 10, 11]), 3, df["season"])).astype("int8")
+    df['pay_day'] = np.where((df['day_of_month']==15) | (df['is_month_end']==1), 1, 0)
+    df['earthquake_impact'] = np.where(df['date'].isin(
+        pd.date_range(start='2016-04-16', end='2016-12-31', freq='D')), 1, 0)
+
+    return df
+
+input_df = getDateFeatures(input_df)
+input_df = input_df.drop(columns= ['date'], axis=1)
 
 # Make prediction and show results
 if st.button('Predict'):
-  prediction = predict_sales(input_df.values)
-  st.success('The predicted sales amount is $' + str(round(prediction,2)))
+  prediction = predict_sales(input_df.values, input_df)
+  st.success('The predicted sales amount is $' + str(round(prediction[0],2)))
